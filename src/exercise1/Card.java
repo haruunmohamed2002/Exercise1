bf1e2098a1107c37618d584a6840a05398dc1eb8
haruunmohamed2002 <haruunmohamed2002@gmail.com>
2021-01-26 20:06:50 -0500

    Created Suits array and setter

--- src/exercise1/Card.java
+++ src/exercise1/Card.java
@@ -10,5 +10,13 @@ package exercise1;
  * @author swage
  */
 public class Card {
+    private int Cardnum;
+   
+    
+    public static final String [] suit = {"Clubs", "Diamonds", "Spades", "Hearts"};
+
+    public void setCardnum(int Cardnum) {
+        this.Cardnum = Cardnum;
+    }
     
 }
--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -5,6 +5,8 @@
  */
 package exercise1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Haruun Mohamed Jan 26 2021
@@ -15,7 +17,19 @@ public class CardTrick {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+      
+        for(int i = 0; i < suits.length; i++){
+        
+        
+        
+        
+        
+        
+        }
+        
+        
+       
     }
     
 }
